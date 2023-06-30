b4f09b4f3e351a302ba70b0b4dc1e6eb2181eda0
zsxkib <zsakib.ahamed@gmail.com>
2023-06-30 22:11:46 +0000

    code clean up

--- predict.py
+++ predict.py
@@ -1,20 +1,25 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from typing import List
-from cog import BasePredictor, Input, Path
-import os
+# Standard library imports
 import shutil
-import torch
-import numpy as np
-import imageio
-from PIL import Image
 import uuid
+from typing import Any, Dict, Iterator, List, Optional, Tuple
+
+# Related third-party imports
+import imageio
+import numpy as np
+import torch
+from PIL import Image
+from PIL.Image import Image as PILImage  # For Typing PIL.Image.Image
+from cog import BasePredictor, Input, Path
 from tqdm import tqdm
 
+# Local application/library specific imports
 from draggan import utils
 from draggan.draggan import drag_gan
 from draggan import draggan as draggan
+
 
 SIZE_TO_CLICK_SIZE = {1024: 8, 512: 5, 256: 2}
 CKPT_SIZE = {
@@ -43,21 +48,27 @@ OUTPUT_DIR = Path("./out")
 
 
 class Predictor(BasePredictor):
-    def setup(self):
+    def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
+
         self.device = "cuda"
         self.init_output_dir()
+        self.output_dir = OUTPUT_DIR
         self.load_model(DEFAULT_CKPT)
 
-    def init_output_dir(self):
-        for child in OUTPUT_DIR.iterdir():
+    def init_output_dir(self) -> None:
+        """Creates/Recreates the output folder where all the images and videos go"""
+
+        for child in self.output_dir.iterdir():
             if child.is_file():
                 child.unlink()
             else:
                 shutil.rmtree(child)
-        OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+        self.output_dir.mkdir(parents=True, exist_ok=True)
 
-    def load_model(self, checkpoint=DEFAULT_CKPT):
+    def load_model(self, checkpoint: str = DEFAULT_CKPT) -> None:
+        """Loads the model given a checkpoint path"""
+
         self.G = draggan.load_model(utils.get_path(checkpoint), device=self.device)
         self.W = draggan.generate_W(
             self.G,
@@ -70,11 +81,28 @@ class Predictor(BasePredictor):
         self.model = {"G": self.G}
         self.current_checkpoint = checkpoint
 
-    def add_points_to_image(self, image, points, point_size=5):
+    def add_points_to_image(
+        self,
+        image: PILImage,
+        points: Dict[str, List[List[int]]],
+        point_size: int = 5,
+    ) -> PILImage:
+        """Adds the points, lines, and arrows to the image"""
+
         image = utils.draw_handle_target_points(image, points["handle"], points["target"], point_size)
         return image
 
-    def on_drag(self, model, points, max_iters, state, size, mask, lr_box):
+    def on_drag(
+        self,
+        points: Dict[str, List[List[int]]],
+        max_iters: int,
+        state: Dict[str, Any],
+        size: int,
+        mask: Dict[str, Optional[Any]],
+        lr_box: float,
+    ) -> Iterator[Tuple[np.ndarray, Dict[str, Any], int]]:
+        """The magic drag function!"""
+
         if len(points["handle"]) == 0:
             raise Exception("You must select at least one handle point and target point.")
         if len(points["handle"]) != len(points["target"]):
@@ -98,7 +126,13 @@ class Predictor(BasePredictor):
 
         step = 0
         for image, W, handle_points in drag_gan(
-            W, model["G"], handle_points, target_points, mask, max_iters=max_iters, lr=lr_box
+            W,
+            self.model["G"],
+            handle_points,
+            target_points,
+            mask,
+            max_iters=max_iters,
+            lr=lr_box,
         ):
             points["handle"] = [p.cpu().numpy().astype("int") for p in handle_points]
             image = self.add_points_to_image(image, points, point_size=SIZE_TO_CLICK_SIZE[size])
@@ -106,7 +140,7 @@ class Predictor(BasePredictor):
             state["history"].append(image)
             step += 1
 
-            output_path = OUTPUT_DIR / f"output_{step}.png"
+            output_path = self.output_dir / f"output_{step}.png"
             Image.fromarray(image).save(output_path)
 
             yield (image, state, step)
@@ -178,15 +212,15 @@ class Predictor(BasePredictor):
         max_iters = 1 if just_render_first_frame else max_iters
         lr_box = 0 if just_render_first_frame else lr_box
         f = (
-            OUTPUT_DIR / f"output_1.png"
+            self.output_dir / f"output_1.png"
             if just_render_first_frame
-            else OUTPUT_DIR / f"video_{uuid.uuid4()}.mp4"
+            else self.output_dir / f"video_{uuid.uuid4()}.mp4"
         )
 
         frames = [
             image
             for image, _, _ in tqdm(
-                self.on_drag(self.model, points, max_iters, state, size, mask, lr_box),
+                self.on_drag(points, max_iters, state, size, mask, lr_box),
                 total=max_iters,
             )
         ]
