6970f74dd5301a31449fef1b6afbddaaf3b76498
zsxkib <zsakib.ahamed@gmail.com>
2023-06-30 12:53:11 +0000

    cog building + added cuda version

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@
 
 from cog import BasePredictor, Input, Path
 import os
-import gradio as gr
+import shutil
 import torch
 import numpy as np
 import imageio
@@ -37,59 +37,51 @@ CKPT_SIZE = {
     "self_distill/parrots_512_pytorch.pkl": 512,
 }
 DEFAULT_CKPT = "ada/afhqcat.pkl"
+OUTPUT_DIR = "./out/"
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
 
-        device = "cuda"
+        self.device = "cuda"
 
-        G = draggan.load_model(utils.get_path(DEFAULT_CKPT), device=device)
-        W = draggan.generate_W(
-            G,
+        shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+        os.makedirs(OUTPUT_DIR)
+
+        self.G = draggan.load_model(utils.get_path(DEFAULT_CKPT), device=self.device)
+        self.W = draggan.generate_W(
+            self.G,
             seed=int(1),
-            device=device,
+            device=self.device,
             truncation_psi=0.8,
             truncation_cutoff=8,
         )
-        img, F0 = draggan.generate_image(W, G, device=device)
-        model = {"G": G}
+        self.img, self.F0 = draggan.generate_image(self.W, self.G, device=self.device)
+        self.model = {"G": self.G}
 
-    def predict(
-        self,
-        stylegan2_model: str = Input(
-            description="Select a StyleGAN2 model", choices=list(CKPT_SIZE.keys()), default=DEFAULT_CKPT
-        ),
-        seed: int = Input(description="Specify the seed for image generation", ge=0, le=100, default=1),
-        learning_rate: float = Input(
-            description="Specify the learning rate for the drag operation", ge=1e-4, le=1, default=2e-3
-        ),
-        max_iterations: int = Input(
-            description="Specify the maximum iterations for the drag operation", ge=1, le=500, default=20
-        ),
-    ) -> Path:
-        """Run a single prediction on the model"""
-        model, points, max_iters, state, size, mask, lr_box
+    def add_points_to_image(self, image, points, size=5):
+        image = utils.draw_handle_target_points(image, points["handle"], points["target"], size)
+        return image
 
+    def on_drag(self, model, points, max_iters, state, size, mask, lr_box):
         if len(points["handle"]) == 0:
             raise Exception("You must select at least one handle point and target point.")
         if len(points["handle"]) != len(points["target"]):
             raise Exception(
                 "You have uncompleted handle points, try to selct a target point or undo the handle point."
             )
-
         max_iters = int(max_iters)
         W = state["W"]
 
-        handle_points = [torch.tensor(p, device=device).float() for p in points["handle"]]
-        target_points = [torch.tensor(p, device=device).float() for p in points["target"]]
+        handle_points = [torch.tensor(p, device=self.device).float() for p in points["handle"]]
+        target_points = [torch.tensor(p, device=self.device).float() for p in points["target"]]
 
-        if mask.get("mask") is not None:
+        if mask.get("mask", None) is not None:
             mask = Image.fromarray(mask["mask"]).convert("L")
             mask = np.array(mask) == 255
 
-            mask = torch.from_numpy(mask).float().to(device)
+            mask = torch.from_numpy(mask).float().to(self.device)
             mask = mask.unsqueeze(0).unsqueeze(0)
         else:
             mask = None
@@ -99,22 +91,48 @@ class Predictor(BasePredictor):
         os.makedirs(output_directory, exist_ok=True)
 
         for image, W, handle_points in drag_gan(
-            W,
-            model["G"],
-            handle_points,
-            target_points,
-            mask,
-            max_iters=max_iters,
-            lr=lr_box,
+            W, model["G"], handle_points, target_points, mask, max_iters=max_iters, lr=lr_box
         ):
             points["handle"] = [p.cpu().numpy().astype("int") for p in handle_points]
-            image = add_points_to_image(image, points, size=SIZE_TO_CLICK_SIZE[size])
+            image = self.add_points_to_image(image, points, size=SIZE_TO_CLICK_SIZE[size])
 
             state["history"].append(image)
             step += 1
 
-            # Save image to disk
             output_path = os.path.join(output_directory, f"output_{step}.png")
             Image.fromarray(image).save(output_path)
 
             yield image, state, step
+
+    def predict(
+        self,
+        # stylegan2_model: str = Input(
+        #     description="Select a StyleGAN2 model", choices=list(CKPT_SIZE.keys()), default=DEFAULT_CKPT
+        # ),
+        # seed: int = Input(description="Specify the seed for image generation", ge=0, le=100, default=1),
+        lr_box: float = Input(
+            description="Specify the learning rate for the drag operation", ge=1e-4, le=1, default=2e-3
+        ),
+        max_iters: int = Input(
+            description="Specify the maximum iterations for the drag operation", ge=1, le=500, default=20
+        ),
+    ) -> Path:
+        """Run a single prediction on the model"""
+
+        shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+        os.makedirs(OUTPUT_DIR)
+
+        points = {"target": [[309, 57]], "handle": [[379, 247]]}  # TODO: add as param
+        state = {"W": self.W, "history": []}
+        size = 512  # TODO: add as param
+        mask = {}
+
+        # NOTE: We wrap in list to "wait" for Generator (on_drag)
+        _ = list(self.on_drag(self.model, points, max_iters, state, size, mask, lr_box))
+
+        output_paths = [Path(OUTPUT_DIR) / file for file in os.listdir(OUTPUT_DIR) if file.endswith(".png")]
+
+        if len(output_paths) == 0:
+            raise Exception("No output images found. Please check the process.")
+
+        return output_paths
